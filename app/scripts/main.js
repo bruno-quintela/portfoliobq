18f06234c0ec024374a031ffed15a3fb597ae874
brunoQuintela <bruno.quintela.bastos@gmail.com>
2014-10-24 22:14:53 +0000

    updated threeJS to r69

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -340,7 +340,7 @@ Page = function() {
                     vertexShader: ['varying vec2 vUv;', 'void main() {', 'vUv = vec2( uv.x, uv.y );', 'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );', '}'].join('\n'),
                     fragmentShader: ['uniform float mixRatio;', 'uniform sampler2D tDiffuse1;', 'uniform sampler2D tDiffuse2;', 'uniform sampler2D tMixTexture;', 'uniform int useTexture;', 'uniform float threshold;', 'varying vec2 vUv;', 'void main() {', 'vec4 texel1 = texture2D( tDiffuse1, vUv );', 'vec4 texel2 = texture2D( tDiffuse2, vUv );', 'if (useTexture==1) {', 'vec4 transitionTexel = texture2D( tMixTexture, vUv );', 'float r = mixRatio * (1.0 + threshold * 2.0) - threshold;', 'float mixf=clamp((transitionTexel.r - r)*(1.0/threshold), 0.0, 1.0);', 'gl_FragColor = mix( texel1, texel2, mixf );', '} else {', 'gl_FragColor = mix( texel2, texel1, mixRatio );', '}', '}'].join('\n')
                 });
-                var quadgeometry = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight);
+                var quadgeometry = new THREE.PlaneBufferGeometry(window.innerWidth, window.innerHeight);
                 this.quad = new THREE.Mesh(quadgeometry, this.quadmaterial);
                 this.scene.add(this.quad);
                 // Link both scenes and their FBOs
